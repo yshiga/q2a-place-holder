b578d1660566dabf352ed4179885b91dc1236bd8
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-20 17:58:47 +0900

    [mod]Modify set_placeholder function

--- qa-place-holder-layer.php
+++ qa-place-holder-layer.php
@@ -6,24 +6,22 @@ class qa_html_theme_layer extends qa_html_theme_base
 	{
 		$fields = $this->content['form']['fields'];
 		if ($this->template == 'register') {
-			$fields = $this->set_placeholder($fields, 'handle');
-			$fields = $this->set_placeholder($fields, 'password');
-			$fields = $this->set_placeholder($fields, 'email');
+			$this->set_placeholder($fields, 'handle');
+			$this->set_placeholder($fields, 'password');
+			$this->set_placeholder($fields, 'email');
 		} elseif (qa_is_logged_in() && $this->template == 'ask') {
-			$fields = $this->set_placeholder($fields, 'title');
-			// $this->set_placeholder($fields, 'content');
+			$this->set_placeholder($fields, 'title');
 		}
 		$this->content['form']['fields'] = $fields;
 		qa_html_theme_base::main();
 	}
 
-	private function set_placeholder($fields = null, $name = '')
+	private function set_placeholder(&$fields = null, $name = '')
 	{
 		if (isset($fields[$name])) {
 			$placeholder = ' placeholder="' . qa_lang_html('qa_place_holder_lang/' . $name) . '"';
 			$fields[$name]['tags'] .= $placeholder;
 		}
-		return $fields;
 	}
 
 }
